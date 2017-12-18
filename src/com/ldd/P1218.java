594ab9ce3b39a38f7a91fc43e2b0a23ded790714
dandanBrave <269045975@qq.com>
2017-12-18 17:03:42 +0800

    first commit

---
+++ src/com/ldd/P1218.java
@@ -0,0 +1,25 @@
+package com.ldd;
+
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2017/12/18.
+ */
+public class P1218 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            String s = sc.nextLine();
+            int sum = 0;
+            int i = s.length() - 1;
+            while (s.charAt(i)!=' ' && i>=0){
+                sum ++;
+                if(i==0){
+                    break;
+                }
+                i--;
+            }
+            System.out.println(sum);
+        }
+    }
+}
