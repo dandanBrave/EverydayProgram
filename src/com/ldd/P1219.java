3dada9bb827e746309d3010327b70910ad63fd71
dandanBrave <269045975@qq.com>
2017-12-19 10:47:46 +0800

    second day

--- src/com/ldd/P1219.java
+++ src/com/ldd/P1219.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 /**
  * Created by Administrator on 2017/12/19.
+ * 计算字符个数:写出一个程序，接受一个有字母和数字以及空格组成的字符串，和一个字符，然后
+ * 输出输入字符串中含有该字符的个数。不区分大小写
  */
 public class P1219 {
     public static void main(String[] args) {
