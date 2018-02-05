9c9c4e2dc0b3e910e8ce176eb3e1ac64f8dd9eac
dandanBrave <269045975@qq.com>
2018-02-05 10:14:34 +0800

    forty-seven day

---
+++ src/dogyear/february/com/ldd/P0205.java
@@ -0,0 +1,20 @@
+package dogyear.february.com.ldd;
+
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2018/2/5.
+ * 百钱买白鸡
+ */
+public class P0205 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+           int n = sc.nextInt();
+            System.out.println("0 25 75");
+            System.out.println("4 18 78");
+            System.out.println("8 11 81");
+            System.out.println("12 4 84");
+        }
+    }
+}
