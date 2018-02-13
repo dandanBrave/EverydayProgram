5b29d990f1b341d5c4de9e1f6d38d2963386c016
dandanBrave <269045975@qq.com>
2018-02-13 23:10:21 +0800

    fifty-four day

---
+++ src/dogyear/february/com/ldd/P0213.java
@@ -0,0 +1,51 @@
+package dogyear.february.com.ldd;
+
+/**
+ * Created by Administrator on 2018/2/13.
+ */
+public class P0213 {
+    public static void main(String[] args) {
+
+    }
+    public RandomListNode Clone(RandomListNode pHead)
+    {
+        RandomListNode p=pHead;
+        RandomListNode t=pHead;
+        while(p!=null){
+            RandomListNode q=new RandomListNode(p.label);
+            q.next=p.next;
+            p.next=q;
+            p=q.next;
+        }
+        while(t!=null){
+            RandomListNode q=t.next;
+            if(t.random!=null)
+                q.random=t.random.next;
+            t=q.next;
+
+        }
+        RandomListNode s=new RandomListNode(0);
+        RandomListNode s1=s;
+        while(pHead!=null){
+            RandomListNode  q=pHead.next;
+            pHead.next=q.next;
+            q.next=s.next;
+            s.next=q;
+            s=s.next;
+            pHead=pHead.next;
+
+
+        }
+        return s1.next;
+
+    }
+    public class RandomListNode {
+        int label;
+        RandomListNode next = null;
+        RandomListNode random = null;
+
+        RandomListNode(int label) {
+            this.label = label;
+        }
+    }
+}
